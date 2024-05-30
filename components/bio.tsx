d9f9ca7f952d5ee1c6b30df2aa234fea86e63e2a
M@W <mehdicr7wahid@gmail.com>
2024-05-30 20:05:54 +0430

    created the about page

---
+++ components/bio.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Bio() {
+  return (
+    <div>
+      <h1 className="font-bold text-lg">Hello!</h1>
+      <p>
+        I'm Brian, a software developer and educator living in Boston,
+        Massachusetts. I run Spanning Tree, a video series teaching topics in
+        computer science and mathematics. At Harvard University, I've created
+        and taught courses about artificial intelligence and web programming.
+        I've also been a course head for CS50, Harvard's introductory course in
+        computer science, and a co-instructor for CS51, Harvard's course on
+        abstraction and design in computing. I currently work at Byteboard,
+        developing a more accurate and equitable process for engineering hiring.
+        I've worked previously at Automattic, Palantir, and the National Speech
+        and Debate Association. I graduated from Harvard College in 2019 with a
+        degree in computer science and linguistics and from Harvard Graduate
+        School of Education in 2021 with a master's degree in technology,
+        innovation, and education. Contact me at brian@brianyu.me.
+      </p>
+    </div>
+  );
+}
+
+export default Bio;
