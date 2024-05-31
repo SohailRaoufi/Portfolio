dcf3ca9316447578a657291f75d5a544e6d2a1a2
Masoom-Wahid <masoom3892@gmail.com>
2024-05-31 13:12:47 +0430

    edited bio3

--- components/bio.tsx
+++ components/bio.tsx
@@ -8,7 +8,7 @@ function Bio() {
       I&apos;m Masoom Wahid, a Backend developer and Competitive programmer.
       Currently, I&apos;m expanding my knowledge by studying design principles and diving deeper into the world of competitive programming through the Competitive Programming Handbook.
       With a strong foundation in programming languages, I&apos;m proficient in Django, Python, Go, and Rust.
-      In the past, I&apos;ve had the opportunity to achieve a notable feat - I secured the 10th position in the West Asia ICPC competition.
+      In the past,I secured the 10th position in the West Asia ICPC competition.
       I&apos;m passionate about staying up-to-date with the latest developments in the field and exploring new technologies to improve my skills.
       </p>
     </div>
