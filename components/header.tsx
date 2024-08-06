682a5d6ba034fab6100c6b9b8a33af2906174d9a
Masoom-Wahid <masoom3892@gmail.com>
2024-08-06 20:29:53 +0430

    changed image to rounded

--- components/header.tsx
+++ components/header.tsx
@@ -19,7 +19,11 @@ function Header({
 }) {
   return (
     <div className="flex flex-col items-center">
-      <Image src={profile} className="w-64 h-64" alt="profile picture" />
+      <Image
+        src={profile}
+        className="w-64 h-64 rounded-full"
+        alt="profile picture"
+      />
       <h1 className="font-bold text-[3rem] text-center mt-4">Masoom Wahid</h1>
       <TypeAnimation
         sequence={[
