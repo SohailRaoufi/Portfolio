44e9d2e70cc758ba45d2ea028a889d7b979cf788
Masoom-Wahid <masoom3892@gmail.com>
2024-11-28 00:43:45 +0430

    again idk

--- components/bio.tsx
+++ components/bio.tsx
@@ -6,9 +6,9 @@ function Bio() {
       <h1 className="font-bold text-[2rem]">Hello!</h1>
       <p className="text-[1.25rem] leading-[1.65]">
         I&apos;m Masoom Wahid, a Backend developer/Rust Dev and Competitive programmer.
-        Currently, I&apos;m expanding my knowledge by &apos;Designing Data Intensive Applications&apos; and diving deeper into the world of competitive programming through the Competitive Programming Handbook.
+        Currently, I&apos;am studying system design and assembly. 
         With a strong foundation in programming languages, I&apos;m proficient in Django, Python, Golang, and Rust and TypeScript.
-        In the past,I secured the 10th position in the West Asia ICPC competition.
+        I have secured the 1th position in the West Asia ICPC competition.
         I&apos;m passionate Compilers and Interpeters, hence Why I am Working On Creating One on My Own.
       </p>
     </div>
